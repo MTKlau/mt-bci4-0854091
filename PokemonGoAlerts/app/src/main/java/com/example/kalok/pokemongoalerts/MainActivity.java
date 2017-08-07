0d0fee128cfda61e2a824537ec327927e7cb2745
kalok <kaloklau88@gmail.com>
2017-08-07 23:48:32 +0200

    Added the necessary views and permissions

---
+++ PokemonGoAlerts/app/src/main/java/com/example/kalok/pokemongoalerts/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.kalok.pokemongoalerts;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.LocationSource;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements LocationSource.OnLocationChangedListener{
+
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+
+    private TextView nameTextView;
+    private TextView emailTextView;
+    private TextView passwordTextView;
+    private TextView passwordConfirmTextView;
+
+    private boolean isNetworkEnabled;
+    private Location location;
+    private LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void askForLocationPermission() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (isNetworkEnabled) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_FINE_LOCATION);
+            }
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if(location != null){
+                Log.d("latitude",location.getLatitude()+"");
+                Log.d("longitude",location.getLongitude()+"");
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("latitude",location.getLatitude()+"");
+        Log.d("longitude",location.getLongitude()+"");
+    }
+}
