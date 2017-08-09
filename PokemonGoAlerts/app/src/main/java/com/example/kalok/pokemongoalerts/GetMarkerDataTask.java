3fc705f25b2d64ef0e6ff0864cb13ddae364795c
kalok <kaloklau88@gmail.com>
2017-08-09 20:18:00 +0200

    Notification functions added

---
+++ PokemonGoAlerts/app/src/main/java/com/example/kalok/pokemongoalerts/GetMarkerDataTask.java
@@ -0,0 +1,14 @@
+package com.example.kalok.pokemongoalerts;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Kalok on 9-8-2017.
+ */
+
+public class GetMarkerDataTask extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+}
--- PokemonGoAlerts/app/src/main/java/com/example/kalok/pokemongoalerts/MainActivity.java
+++ PokemonGoAlerts/app/src/main/java/com/example/kalok/pokemongoalerts/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kalok.pokemongoalerts;
 
 import android.Manifest;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -9,24 +12,29 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.model.LatLng;
 
-public class MainActivity extends AppCompatActivity implements LocationSource.OnLocationChangedListener{
+public class MainActivity extends AppCompatActivity implements LocationSource.OnLocationChangedListener, View.OnClickListener{
 
     private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+    private static final int uniqueID = 345345;
 
     private EditText nameEditText;
     private EditText emailEditText;
     private EditText passwordEditText;
     private EditText passwordConfirmEditText;
+    private Button submitAccountButton;
+    private NotificationCompat.Builder notification;
 
     private boolean isNetworkEnabled;
     private Location location;
@@ -42,7 +50,24 @@ public class MainActivity extends AppCompatActivity implements LocationSource.On
         nameEditText = (EditText) findViewById(R.id.nameEditText);
         emailEditText = (EditText) findViewById(R.id.emailEditText);
         passwordEditText = (EditText) findViewById(R.id.passwordEditText);
-        passwordConfirmEditText = (EditText) findViewById(R.id.passwordConfirmedEditText;)
+        passwordConfirmEditText = (EditText) findViewById(R.id.passwordConfirmedEditText);
+        submitAccountButton = (Button) findViewById(R.id.submitAccountButton);
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        notification.setTicker("Ticker");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Content title");
+        notification.setContentText("Content body");
+
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(uniqueID,notification.build());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -83,5 +108,15 @@ public class MainActivity extends AppCompatActivity implements LocationSource.On
     public void onLocationChanged(Location location) {
         Log.d("latitude",location.getLatitude()+"");
         Log.d("longitude",location.getLongitude()+"");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.submitAccountButton:
+                break;
+            default:
+                break;
+        }
     }
 }
