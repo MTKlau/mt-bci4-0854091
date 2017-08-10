ca2a69015a057f46e1e739e1338aaa618f19d91c
kalok <kaloklau88@gmail.com>
2017-08-10 23:44:54 +0200

    Fixed some bugs in the activities

--- PokemonGoAlerts/app/src/main/java/com/example/kalok/pokemongoalerts/HomeActivity.java
+++ PokemonGoAlerts/app/src/main/java/com/example/kalok/pokemongoalerts/HomeActivity.java
@@ -1,13 +1,17 @@
 package com.example.kalok.pokemongoalerts;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button goToMapsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,20 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        goToMapsButton = (Button) findViewById(R.id.goToMapsButton);
+        goToMapsButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case(R.id.goToMapsButton):
+                Intent mapIntent = new Intent(HomeActivity.this,MapsActivity.class);
+                startActivity(mapIntent);
+                break;
+            default:
+                break;
+        }
+    }
 }
